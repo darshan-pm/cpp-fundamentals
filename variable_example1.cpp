6702a81613e9ea548ac298b5e071371684f7e470
Darshan <darshan200713@gmail.com>
2026-03-31 21:43:07 +0530

    added variable example1 program

---
+++ variable_example1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x = 7;
+    int y = 8;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+    return 0;
+}
