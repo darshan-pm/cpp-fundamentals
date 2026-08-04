0b3078551ea0409c2c8ef65145d4ce6c9988c81c
Darshan <darshan200713@gmail.com>
2026-08-04 18:16:58 +0530

    added comment practice program

---
+++ comment.cpp
@@ -0,0 +1,9 @@
+// this is the preproccessor and standard library
+#include <iostream>
+/*this is the main function
+program starts from here */
+int main() {
+    std::cout << "Hey there" << std::endl;
+    // it tells the program executed successfully
+    return 0;
+}
