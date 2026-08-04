0bf31ed987fec38014d41dc37fed656b77d93f9a
Darshan <darshan200713@gmail.com>
2026-08-04 18:37:00 +0530

    added main example1

---
+++ main_example1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Number1" << std ::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Darshan" << std::endl;
+    std::cout << "Bhargav" << std::endl;
+    std::cout << "Adrusht" << std::endl;
+    std::cout << "Pranav" << std::endl;
+    return 0;
+}
