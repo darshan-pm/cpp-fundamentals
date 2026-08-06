2c52c51ef2f3f3a5e9d404d182d7010aaa7082c3
Darshan <darshan200713@gmail.com>
2026-08-06 21:16:55 +0530

    added setw practice program 2

---
+++ setw_practice2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    std::cout << "Name" << std::setw(10) << "Age" << std::setw(10) << std::endl;
+    std::cout << "Darshan" << std::setw(10) << "19" << std::setw(10) << std::endl;
+    std::cout << "Keerthi" << std::setw(10) << "19" << std::setw(10) << std::endl;
+    std::cout << "Yuktha" << std::setw(10) << "19" << std::setw(10) << std::endl;
+    std::cout << "Niharika" << std::setw(10) << "19" << std::setw(10) << std::endl;
+    return 0;
+}
