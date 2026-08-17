185a9e8e354354e2c6c92d863af7d1ffb064a72b
Darshan <darshan200713@gmail.com>
2026-08-17 08:11:06 +0530

    added variable example2 program

---
+++ variable_example2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int age(35);
+    int weight(60.5);
+    int height(5.4);
+
+    std::cout << "age is : " << age << std::endl;
+    std::cout << "wieght is : " << weight << std::endl;
+    std::cout << "height is: " << height << std::endl;
+    return 0;
+}
